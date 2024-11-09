0a59b44219fc1294126a0a55064c320925f3e65a
GopiManiKiran <gorantlagopimanikiran2005@gmail.com>
2024-11-09 21:30:42 +0530

    Update api.js

--- Final travel app/fta/src/api.js
+++ Final travel app/fta/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://final-travel-app-backend.onrender.com/api' });
 
 export const createTrip = (tripData) => API.post('/trips', tripData);
