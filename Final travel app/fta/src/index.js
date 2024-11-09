727400ea91ca2f622f6969aecd7363c44ab1ea79
GopiManiKiran <gorantlagopimanikiran2005@gmail.com>
2024-11-09 22:08:57 +0530

    Update index.js

--- Final travel app/fta/src/index.js
+++ Final travel app/fta/src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-import { BrowserRouter, Routes, Route, createBrowserRouter } from 'react-router-dom';
-
-const router = createBrowserRouter({
-  future: {
-    v7_relativeSplatPath: true, // Opt-in to v7's relative splat path behavior
-  },
-});
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
